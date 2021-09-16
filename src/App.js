895b00c636d45fa0252580a8266887aa834c8d1f
zerow41 <dudtj0410@naver.com>
2021-09-16 11:14:17 +0900

    Update css

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading 
-          ? (<div class="loader">
-              <span class="loader_text">Loading...</span> 
+          ? (<div classNAme="loader">
+              <span className="loader_text">Loading...</span> 
             </div>
           ) : (
-            <div class="movies">
+            <div className="movies">
               {movies.map(movie => (
                 <Movie 
                   key={movie.id}
@@ -32,6 +32,7 @@ class App extends React.Component {
                   title={movie.title} 
                   summary={movie.summary} 
                   poster={movie.medium_cover_image} 
+                  genres={movie.genres}
                 />
               ))}
             </div>
